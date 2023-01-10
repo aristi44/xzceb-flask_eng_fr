95326cefcfe124719e708d66c5bdf3affdd3b27a
aristi44 <scacco.aristi@gmail.com>
2023-01-10 16:32:32 -0500

    Temporary changes

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,14 @@
+import unittest
+from translator import englishtofrench, frenchtoenglish
+
+class TestEnglishToFrench(unittest.TestCase):
+    def test1(self):
+        self.assertNotEqual(englishtofrench("hello"),"hola")
+        self.assertEqual(englishtofrench("hello"),"Bonjour")
+
+class TestFrenchToEnglish(unittest.TestCase):
+    def test1(self):
+        self.assertNotEqual(frenchtoenglish("Bonjour"),"hola")
+        self.assertEqual(frenchtoenglish("Bonjour"),"hello")
+        
+unittest.main()
